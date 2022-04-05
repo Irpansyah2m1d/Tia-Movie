8fba4b34493367e9e1d3b57dc1e5fc740a148606
irpansyah2m1d <irpansyah810@gmail.com>
2022-04-05 17:52:06 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$("#button-search").on("click", function () {
+  $("#movie-list").html("");
+  let keyword = $("#input-search").val();
+  $.ajax({
+    url: "https://www.omdbapi.com",
+    dataType: "json",
+    type: "get",
+    data: {
+      apikey: "6364b8a7",
+      s: keyword,
+    },
+    success: function (data) {
+      if (data.Response == "True") {
+        let movie = data.Search;
+        $.each(movie, function (i, data) {
+          $("#movie-list").append(
+            `
+            <div class="col-lg-3">
+                <div class="card mb-3" >
+                    <img src="` +
+              data.Poster +
+              `" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">` +
+              data.Title +
+              `</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Tahun Realesed : ` +
+              data.Year +
+              `</h6>
+              <a href="#" class="card-link" id="see-detail">See Detail</a>
+                    </div>
+                </div>
+          </div>
+          `
+          );
+        });
+      } else {
+        $("#movie-list").html(`
+            <div class="col">
+                <h3 class="text-center">Film Tidak Ditemukan Bestiee!!!</h3>
+            </div>
+        `);
+      }
+    },
+  });
+});

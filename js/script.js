854758a8ac1a5e6ffba5154850707bc613601b31
irpansyah2m1d <irpansyah810@gmail.com>
2022-04-06 14:30:00 +0700

    Fitur Loading, enter, see detail

--- js/script.js
+++ js/script.js
@@ -8,7 +8,10 @@ $("#input-search").on("keyup", function (e) {
 });
 
 function movieSearch() {
-  $("#movie-list").html("");
+  $("#movie-list").html(
+    `<img src="img/loader.gif" alt="loader" class="loader-gif d-none mx-auto position-absolute top-0" />`
+  );
+  $(".loader-gif").removeClass("d-none");
   let keyword = $("#input-search").val();
   $.ajax({
     url: "https://www.omdbapi.com",
@@ -19,6 +22,11 @@ function movieSearch() {
       s: keyword,
     },
     success: function (data) {
+      $("#input-search").val("");
+      $("#movie-list").html(
+        `<img src="img/loader.gif" alt="loader" class="loader-gif d-none mx-auto position-absolute top-0" />`
+      );
+      $("#movie-list .loader-gif").hide();
       if (data.Response == "True") {
         let movie = data.Search;
         $.each(movie, function (i, data) {
@@ -36,7 +44,9 @@ function movieSearch() {
               <h6 class="card-subtitle mb-2 text-muted">Tahun Realesed : ` +
               data.Year +
               `</h6>
-              <a href="#" class="card-link" id="see-detail">See Detail</a>
+              <a href="#" class="card-link" id="see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="` +
+              data.imdbID +
+              `">See Detail</a>
                     </div>
                 </div>
           </div>
@@ -53,3 +63,45 @@ function movieSearch() {
     },
   });
 }
+
+$("#movie-list").on("click", ".card-link", function () {
+  $.getJSON("https://www.omdbapi.com/?apikey=6364b8a7&i=" + $(this).data("id"), function (data) {
+    $(".modal-body").html(
+      `
+    <div class="container">
+        <div class="row justify-content-between">
+        <div class="col-lg-3">
+            <img src="` +
+        data.Poster +
+        `" alt="Movie Photos" style="width: 18rem; height:18rem;" class="mb-3 mx-auto d-block"/>
+        </div>
+        <div class="col-lg-7">
+            <ul class="list-group">
+                    <li class="list-group-item"><b>Judul : </b>` +
+        data.Title +
+        ` </li>
+                    <li class="list-group-item"><b>Language : </b> ` +
+        data.Language +
+        ` </li>
+                    <li class="list-group-item"><b>Actor : </b>` +
+        data.Actors +
+        ` </li>
+                    <li class="list-group-item"><b>Director : </b>` +
+        data.Director +
+        ` </li>
+                    <li class="list-group-item"><b>Genre : </b>` +
+        data.Genre +
+        ` </li>
+                    <li class="list-group-item"><b>Realesed : </b>` +
+        data.Released +
+        ` </li>
+            </ul>
+        </div>
+        </div>
+    </div>
+
+    `
+    );
+    // console.log(data);
+  });
+});
